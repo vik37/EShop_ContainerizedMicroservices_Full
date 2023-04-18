04b255e87948afc8078cfeddf0ce5845d70327f3
vik37 <59177404+vik37@users.noreply.github.com>
2023-04-18 14:54:09 +0200

    Redis Example

--- Services/Basket/EShop.Basket.API/Controllers/WeatherForecastController.cs
+++ Services/Basket/EShop.Basket.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
+using EShop.Basket.API.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
 
 namespace EShop.Basket.API.Controllers
 {
@@ -8,26 +11,54 @@ namespace EShop.Basket.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IDistributedCache _cache;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IDistributedCache cache)
         {
             _logger = logger;
+            _cache = cache;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<RedisExample> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            RedisExample re = new();
+            string recordId = "WeatherForecast_" + DateTime.Now.ToString("yyyyMMdd_hhmm");
+;
+            re.Forecast = await _cache.GetRecordAsync<IEnumerable<WeatherForecast>>(recordId);
+
+            if(re.Forecast is null)
+            {
+                re.Forecast = GetData;
+
+                re.LoadLocation = $"Loaded from API at {DateTime.Now}";
+                await _cache.SetRecordAsync<IEnumerable<WeatherForecast>>(recordId, re.Forecast);
+            }
+            else
+            {
+                re.LoadLocation = $"Loaded from Cache {DateTime.Now}";
+                re.IsCacheData = true;
+            }
+            return re;
+        }
+
+        private static IEnumerable<WeatherForecast> GetData =>
+            Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
-        }
+
+    }
+    public class RedisExample
+    {
+        public string LoadLocation { get; set; }
+        public bool IsCacheData { get; set; } = false;
+        public IEnumerable<WeatherForecast> Forecast { get; set; }
     }
 }
--- Services/Basket/EShop.Basket.API/Program.cs
+++ Services/Basket/EShop.Basket.API/Program.cs
@@ -1,11 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
-
+IConfiguration configuration = builder.Configuration;
+var services = builder.Services;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = configuration.GetConnectionString("Redis");
+    options.InstanceName = "RedisBasket_";
+});
 
 var app = builder.Build();
 
