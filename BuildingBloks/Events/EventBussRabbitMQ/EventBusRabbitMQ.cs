061dcc10c66a0153a1321b221327c29b292556cd
vik37 <59177404+vik37@users.noreply.github.com>
2023-05-18 14:30:46 +0200

    Remove unnecessary package - Autofac.

--- BuildingBloks/Events/EventBussRabbitMQ/EventBusRabbitMQ.cs
+++ BuildingBloks/Events/EventBussRabbitMQ/EventBusRabbitMQ.cs
@@ -3,7 +3,6 @@
 public class EventBusRabbitMQ : IEventBus, IDisposable
 {
     const string BROKER_NAME = "eshop_event_bus";
-    const string AUTOFAC_SCOPE_NAME = "eshop_event_bus";
 
     private readonly IRabbitMQPersistentConnection _persistentConnection;
     private readonly ILogger<EventBusRabbitMQ> _logger;
