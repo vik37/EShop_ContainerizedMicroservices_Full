333dc35a6a57d7f2197aabe96dc35a4d5fa2903a
vik37 <59177404+vik37@users.noreply.github.com>
2023-05-19 18:56:33 +0200

    E-Shop Catalog Unit Testing.

---
+++ Services/Catalog/Catalog.UnitTest/Application/CatalogController_Test.cs
@@ -0,0 +1,98 @@
+﻿namespace Catalog.UnitTest.Application;
+
+public class CatalogController_Test
+{
+    private readonly DbContextOptions<CatalogDbContext> _dbContextOptions;
+    private readonly Mock<ICatalogIntegrationEventService> _eventServiceMock;
+
+    public CatalogController_Test()
+    {
+        _dbContextOptions = new DbContextOptionsBuilder<CatalogDbContext>()
+            .UseInMemoryDatabase("in-memory").Options;
+
+        using var dbContext = new CatalogDbContext(_dbContextOptions);
+        var catalogItem = dbContext.CatalogItems.Any();
+        if (!catalogItem)
+        {
+            dbContext.CatalogItems.AddRange(CatalogFakeDb.FakeCatalog());
+            dbContext.SaveChanges();
+        }
+        _eventServiceMock = new Mock<ICatalogIntegrationEventService>();
+    }
+
+    [Fact]
+    public async Task GetCatalogItems_TestShouldBeSuccess()
+    {        
+        //arrange
+        var brandFilter = 1;
+        var typeFilter = 2;
+        var pageSize = 4;
+        var pageIndex = 1;
+
+        var expectedItemsInPage = 2;
+        var expectedTotalItems = 6;
+
+        var catalogDbContext = new CatalogDbContext(_dbContextOptions);
+
+        //action
+        var catalogController = new CatalogController(catalogDbContext, _eventServiceMock.Object);
+        var actionResult = await catalogController.GetCatalogItemsByBrandAndType(typeFilter, brandFilter, pageSize, pageIndex);
+
+        //assert
+        Assert.IsType<ActionResult<PaginatedItemsDto<CatalogItem>>>(actionResult);
+        var page = Assert.IsAssignableFrom<PaginatedItemsDto<CatalogItem>>(actionResult.Value);
+
+        Assert.Equal(expectedTotalItems, page.Count);
+        Assert.Equal(pageIndex, page.PageIndex);
+        Assert.Equal(pageSize, page.PageSize);
+        Assert.Equal(expectedItemsInPage, page.Data.Count());
+
+    }
+
+    [Fact]
+    public async Task GetCatalogById_TestShouldBeSuccess()
+    {
+        // arrange
+        int catalogId = 3;
+
+        int statusCode = StatusCodes.Status200OK;
+
+        CatalogItem catalogItem = CatalogFakeDb.FakeCatalog().Single(c => c.Id == catalogId);
+
+        string catalogImageUrl = string.Concat("http://localhost:4040/api/v1.0/catalog/items/",$"{catalogItem.PictureFileName!.Split(".")[0]}/image");
+
+        var catalogDbContext = new CatalogDbContext(_dbContextOptions);
+        var integrationServicesMock = new Mock<ICatalogIntegrationEventService>();
+
+        // action
+        var catalogController = new CatalogController(catalogDbContext, _eventServiceMock.Object);
+        var actionResult = (OkObjectResult)await catalogController.ItemByIdAsync(catalogId);
+        var item = actionResult.Value as CatalogItem;
+
+        // assert
+        Assert.True(actionResult.StatusCode.Equals(statusCode));
+        Assert.NotNull(item);
+        Assert.Equal(catalogItem.Name, item!.Name);
+        Assert.Equal(catalogImageUrl, item.PictureUri);
+    }
+
+    [Fact]
+    public async Task GetCatalogById_TestShouldBeFailed_BadRequest()
+    {
+        // arrange
+        int catalogId = 0;
+
+        int statusCode = StatusCodes.Status400BadRequest;
+
+        var catalogDbContext = new CatalogDbContext(_dbContextOptions);
+
+        // action
+        var catalogController = new CatalogController(catalogDbContext,_eventServiceMock.Object);
+        var actionResult = (BadRequestResult) await catalogController.ItemByIdAsync(catalogId);
+
+        // assert
+        Assert.True(actionResult.StatusCode.Equals(statusCode));
+    }
+
+    
+}
---
+++ Services/Catalog/Catalog.UnitTest/Usings.cs
@@ -0,0 +1,15 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.EntityFrameworkCore;
+global using Xunit;
+global using Moq;
+global using EShop.Catalog.API.Controllers;
+global using EShop.Catalog.API.Entities;
+global using EShop.Catalog.API.Infrastructure;
+global using EShop.Catalog.API.IntegrationEvents;
+global using EShop.Catalog.API.Dtos;
+global using Microsoft.AspNetCore.Mvc;
