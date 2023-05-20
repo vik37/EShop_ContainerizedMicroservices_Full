49d28c132c8c23fc91e07f6b899f18247f6320ef
vik37 <59177404+vik37@users.noreply.github.com>
2023-05-20 21:31:19 +0200

    Basket API - Unite Testing.

--- Services/Catalog/Catalog.UnitTest/Usings.cs
+++ Services/Catalog/Catalog.UnitTest/Usings.cs
@@ -1,8 +1,3 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.EntityFrameworkCore;
 global using Xunit;
