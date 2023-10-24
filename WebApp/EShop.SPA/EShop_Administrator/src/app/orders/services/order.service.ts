9efbe43c04eaaa2d6e00d89e50738f0f41056f33
vik37 <59177404+vik37@users.noreply.github.com>
2023-10-24 22:37:32 +0200

    Removing unnecessary function and building Angular.

--- WebApp/EShop.SPA/EShop_Administrator/src/app/orders/services/order.service.ts
+++ WebApp/EShop.SPA/EShop_Administrator/src/app/orders/services/order.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError, retry, catchError, Subject, EMPTY,
+import { throwError, retry, catchError,
         BehaviorSubject } from 'rxjs';
-import {map, tap, switchMap, mergeMap} from 'rxjs/operators';
+import { map, tap, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import {PagginationOrderSummaryViewModel} from 'src/app/orders/models/paggination-order-summary';
-import {OrderSummaryViewModel} from 'src/app/orders/models/order-summary';
 import {Paggination} from 'src/app/shared/models/paggination';
 import {PagginationService} from 'src/app/shared/services/paggination.service';
 
 import {errorMessagesByStatus} from 'src/app/shared/helper/error-messages';
-
 
 @Injectable()
 export class OrderService {
@@ -24,36 +22,15 @@ export class OrderService {
 
   constructor(private http: HttpClient, private pagginationService: PagginationService) { }
 
-
-latestOrder$ = this.http.get(this.API_HTTP+"latest-ordersummary",{'headers':this.headers})
-                          .pipe(
-                            map(data => data),
-                            retry(5),
-                            catchError(this.httpError)
-                          );
+  latestOrder$ = this.http.get(this.API_HTTP+"latest-ordersummary",{'headers':this.headers})
+                            .pipe(
+                              map(data => data),
+                              retry(5),
+                              catchError(this.httpError)
+                            );
 
   $currentPage = new BehaviorSubject<number>(1);
   currPage$ = this.$currentPage.asObservable();
-
-  getOlderOrderSummary(): void{
-    console.log('getOlderOrderSummary() its works');
-    this.currPage$.subscribe(data => console.log('from order service', data))
-    this.currPage$.pipe(
-      switchMap(currentPage => {
-        console.log('from mp',currentPage)
-       return this.http.get(this.API_HTTP+`older-ordersummary?pageSize=5&currentPage=${currentPage}`
-                                        ,{'headers':this.headers})
-      .pipe(
-          tap(data => console.log(data)),
-          map(data => data as PagginationOrderSummaryViewModel),
-          map(data => data as Paggination),
-          map(data => this.pagginationService.pagginationSub.next(data)),
-          retry(5),
-          catchError(this.httpError)
-      )
-      })
-    )
-  }
 
   olderOrderSummary$ = this.$currentPage.pipe(
     switchMap(current =>  this.http
@@ -69,7 +46,9 @@ latestOrder$ = this.http.get(this.API_HTTP+"latest-ordersummary",{'headers':this
           hasNext: data.hasNext,
           hasPrevious: data.hasPrevious
       } as Paggination)),
-      map(data => data.data)
+      map(data => data.data),
+      retry(5),
+      catchError(this.httpError)
     ))
   );
 
